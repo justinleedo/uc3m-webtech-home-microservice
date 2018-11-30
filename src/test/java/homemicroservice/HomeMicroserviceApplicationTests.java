f1973264b125eedc2f1207233c751877671403c4
Justin Do <justinleedo@gmail.com>
2018-11-30 12:39:49 +0000

    Changed application.properties file

--- src/test/java/homemicroservice/HomeMicroserviceApplicationTests.java
+++ src/test/java/homemicroservice/HomeMicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package usermicroservice;
+package homemicroservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UserMicroserviceApplicationTests {
+public class HomeMicroserviceApplicationTests {
 
 	@Test
 	public void contextLoads() {
